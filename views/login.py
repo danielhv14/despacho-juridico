df148898b30149718ff79951cba3c645de200eb2
danielhv14 <danielhv0314@icloud.com>
2026-06-21 23:02:30 -0500

    Agrega pantalla de login

---
+++ views/login.py
@@ -0,0 +1,16 @@
+# views/login.py
+# Pantalla de inicio de sesión
+
+import customtkinter as ctk
+from models import usuario
+from config.settings import (
+    COLOR_PRIMARIO,
+    COLOR_ACENTO,
+    COLOR_FONDO,
+    COLOR_BLANCO,
+    COLOR_TEXTO,
+    COLOR_ERROR,
+    FUENTE_TITULO,
+    FUENTE_NORMAL,
+    APP_NOMBRE,
+)
